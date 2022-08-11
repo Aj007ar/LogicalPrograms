3197c7a598eec602c205986388348f9fcfb0295c
Aj007ar <aj007.ar@gmail.com>
2022-08-11 16:21:44 +0530

    [Ajay] add: Logical file

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n*****************WELCOME TO LOGICAL PROGRAMS*****************");
+        }
+    }
+}
