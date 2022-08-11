8302170638da4eb00c2e5791e4f78e76884b3eb2
Aj007ar <aj007.ar@gmail.com>
2022-08-11 16:39:29 +0530

    [Ajay] add: Fibbonacci file

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,6 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\n*****************WELCOME TO LOGICAL PROGRAMS*****************");
+
+            Console.WriteLine("1.Fibbonacci Number");
+          
+
+            Console.WriteLine("Enter number for execute a program");
+            int check = Convert.ToInt32(Console.ReadLine());
+
+            switch (check)
+            {
+                case 1:
+                    Console.WriteLine("\n********************Fibbonacci Numbers***************************");
+                    Fibbonacci fib = new Fibbonacci();
+                    fib.Fibbo();
+                    break;
+                
+            }
         }
     }
+    
 }
